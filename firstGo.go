6aa6d10bf99a27dba992a36b5698aa1d95b6ffae
nnur1y <nurly125@mail.ru>
2023-02-12 22:01:28 +0600

    new

--- firstGo.go
+++ firstGo.go
@@ -4,6 +4,28 @@ import (
 	"fmt"
 )
 
-func PrintSmth() {
-	fmt.Println("Something")
+type User struct {
+	nickname string
+	email    string
+}
+
+type Recipe struct {
+	name     string
+	calorie  int
+	author   string
+	category string
+}
+
+func UserInfo(nick string, email string) *User {
+	u := User{nick, email}
+	return &u
+}
+
+func (user User) newUser() {
+	fmt.Println("Nickname:", user.nickname, "\nEmail:", user.email)
+}
+
+func RecipeInfo(name string, calorie int, author string, category string) *Recipe {
+	r := Recipe{name, calorie, author, category}
+	return &r
 }
