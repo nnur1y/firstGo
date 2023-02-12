e94f280ae533f57a64d50767fa345401d706a47e
nnur1y <nurly125@mail.ru>
2023-02-12 22:57:25 +0600

    new commit

--- firstGo.go
+++ firstGo.go
@@ -21,7 +21,12 @@ func UserInfo() {
 	fmt.Printf("New user nickname is %s and email - %s", u.nickname, u.email)
 }
 
-func RecipeInfo(name string, calorie int, author string, category string) *Recipe {
+func newRecipe(name string, calorie int, author string, category string) *Recipe {
 	r := Recipe{name, calorie, author, category}
 	return &r
+}
+
+func RecipeInfo() {
+	r := newRecipe("Napoleon", 406, "Nurly", "dessertcake")
+	fmt.Printf("New recipe is %s - %s, it's calorie is %d, \nAuthor - %s", r.category, r.name, r.calorie, r.author)
 }
