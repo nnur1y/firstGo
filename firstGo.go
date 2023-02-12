a86c1cd6e26eda6e46fb4c97cb64ec8d84f6a58e
nnur1y <nurly125@mail.ru>
2023-02-12 23:46:52 +0600

    new commit

--- firstGo.go
+++ firstGo.go
@@ -19,12 +19,13 @@ type Recipe struct {
 func UserInfo() {
 	u := User{"Nurly", "nurly125@mail.ru"}
 	fmt.Printf("New user nickname is %s and email - %s\n", u.nickname, u.email)
+	fmt.Print()
 }
 
 func RecipeInfo() {
 	r := Recipe{"Napoleon", 406, "Nurly", "dessert, cake"}
 	fmt.Printf("New recipe is %s - %s, it's calorie - %d, and Author is %s", r.cat, r.name, r.calorie, r.author)
-
+	fmt.Print()
 }
 
 func AuthorCheck() {
@@ -32,14 +33,16 @@ func AuthorCheck() {
 	r := Recipe{"Napoleon", 406, "Nurly", "dessert, cake"}
 
 	if u.nickname == r.author {
-		fmt.Printf("\nNew User %s is author of %s", u.nickname, r.name)
+		fmt.Printf("New User %s is author of %s", u.nickname, r.name)
 	}
+	fmt.Print()
 }
 
 func CalorieCheck() {
 	r := Recipe{"Napoleon", 406, "Nurly", "dessert, cake"}
 
 	if r.calorie > 400 {
-		fmt.Printf("\nNew %s's calorie is %d, it is bigger than normal", r.name, r.calorie)
+		fmt.Printf("New %s's calorie is %d, it is bigger than normal", r.name, r.calorie)
 	}
+	fmt.Print()
 }
