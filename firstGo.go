f20ecc259051118b056a8e0a5258e6236ac3b6c6
nnur1y <nurly125@mail.ru>
2023-02-12 22:49:16 +0600

    new

--- firstGo.go
+++ firstGo.go
@@ -18,6 +18,10 @@ type Recipe struct {
 
 func UserInfo() {
 	u := User{"Nurly", "nurly125@mail.ru"}
-
 	fmt.Printf("New user nickname is %s and email - %s", u.nickname, u.email)
+}
+
+func RecipeInfo(name string, calorie int, author string, category string) *Recipe {
+	r := Recipe{name, calorie, author, category}
+	return &r
 }
